732825682081e03350873ffcefaae628c61a8602
blueskyusf-code <blueskyusf@gmail.com>
2026-04-01 22:42:24 -0300

    Refactor install.sh for terminal customization
    Removed experimental alert and installation message. Added customization message and updated .bashrc.

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
-echo [*] Alert: This function is experimental and may contain errors.
+echo [*] customizing terminal...
 sleep 2
-echo [*] installing bluesky-tools...
-cat <<EOF>> $PREFIX/bin/bst-inst
-read -p "what install package?: " package
-pkg install \$package || apt install \$package || echo We searched everywhere, but unfortunately we couldn't find the package we were looking for...
-sleep 2
-EOF
+cat <<EOF>> ~/.bashrc
+echo [Plugin, MkDt Form]
+clear
+printf '\e]11;#2e2e2e\a'
+PS1="Terminal@MkDt "
