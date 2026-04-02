7bb04be5dccd4e84ab6d5bf2f9364a6c84703b8a
blueskyusf-code <blueskyusf@gmail.com>
2026-04-01 22:32:48 -0300

    Fix install script to create bst-install file

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 echo [*] Alert: This function is experimental and may contain errors.
 sleep 2
 echo [*] installing bluesky-tools...
-cat <<EOF>> $PREFIX/bin/bst-install
+cat <<EOF>> $PREFIX/bin/bst-inst
 read -p "what install package?: " package
 pkg install \$package || apt install \$package || echo We searched everywhere, but unfortunately we couldn't find the package we were looking for...
 sleep 2
