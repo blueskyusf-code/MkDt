fc2173da571892e39122807341c703eba878a56b
blueskyusf-code <blueskyusf@gmail.com>
2026-04-01 22:45:38 -0300

    Update install.sh to customize terminal settings
    Customize terminal appearance in bashrc for MkDt Edition.

--- install.sh
+++ install.sh
@@ -1,7 +1,14 @@
 echo [*] customizing terminal...
 sleep 2
 cat <<EOF>> ~/.bashrc
+clear
 echo [Plugin, MkDt Form]
+echo
+echo [————————————————————————]
+echo [                        ]
+echo [ termux (Mkdt Edition)  ]
+echo [                        ]
+echo [————————————————————————]
 clear
 printf '\e]11;#2e2e2e\a'
 PS1="Terminal@MkDt "
