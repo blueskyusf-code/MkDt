d23d43601b6fe8aeb6c8707a57ae66ecaa03bbe9
blueskyusf-code <blueskyusf@gmail.com>
2026-04-01 22:31:50 -0300

    Fix install script to use bst-install alias

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
 echo [*] Alert: This function is experimental and may contain errors.
 sleep 2
 echo [*] installing bluesky-tools...
-cat <<EOF>> $PREFIX/bin/install
+cat <<EOF>> $PREFIX/bin/bst-install
 read -p "what install package?: " package
 pkg install \$package || apt install \$package || echo We searched everywhere, but unfortunately we couldn't find the package we were looking for...
 sleep 2
 EOF
-echo "alias bst-install='install'" >> ~/.bashrc
