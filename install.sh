e3959e2715dc596461c833619448e47d52a2c4d6
blueskyusf-code <blueskyusf@gmail.com>
2026-04-02 13:48:01 -0300

    Update install script to use ~/.mkdrc instead of ~/.bashrc

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ cat << 'END' > $PREFIX/bin/MkDt
 #!/data/data/com.termux/files/usr/bin/bash
 echo "[Mkdt Created by Blueskyteam]"
 
-cat << 'EOF' >> ~/.bashrc
+cat << 'EOF' >> ~/.mkdrc
 clear
 printf '\e]11;#2e2e2e\a'
 echo "[Plugin, MkDt Form]"
@@ -22,6 +22,9 @@ PS1="\[\e[1;37m\]Terminal@MkDt \[\e[0m\]"
 EOF
 END
 
-# Essencial para o comando funcionar:
+#P*RRA PRA ATIVAR O MKDRC
+Source ~/.mkdrc
+
+# Essencial para A P*RRA DO COMANDO FUNCIONAR
 chmod +x $PREFIX/bin/MkDt
 	
